3fa2795447fb197846acfacfa44c6a818bf803c9
swaibu <rumbiihas@gmail.com>
2020-12-04 18:32:02 +0300

    (chore):project setup - install webpack - install react - add bootstrap scss

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Assets/scss/style.scss';
+
+const App = () => <div>News Room App</div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
